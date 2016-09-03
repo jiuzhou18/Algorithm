afb752bfff1573ec97bd485561a3019a099537d0
ChaosIllusion <331258298@qq.com>
2016-09-03 12:10:42 +0800

    update

--- Dynamic Programming/src/self_reversalString/MatchingReversalIterately.java
+++ Dynamic Programming/src/self_reversalString/MatchingReversalIterately.java
@@ -2,8 +2,8 @@ package self_reversalString;
 
 public class MatchingReversalIterately {
 	
-	private char[] origin;
-	private String opitmal;
+	private char[] origin = new char[1];
+//	private String optimal;
 	
 	public MatchingReversalIterately (){
 		
@@ -19,5 +19,37 @@ public class MatchingReversalIterately {
 		System.arraycopy(origin, 0, this.origin, 0, origin.length); 
 	}
 	
+	public String splitString(int k){
+		String optimal = "";
+		if (k==0)
+			return optimal = origin[0] +"";
+		else{
+			optimal = splitString(k-1)+" "+origin[k];
+			for (int a=0; a<k; a++){
+				if (origin[a]==origin[k]){
+					boolean judge = true;
+					if(a<k-2){
+						for(int b=a+1; b<=(a+k)/2; b++){
+							if (origin[b]!=origin[a+k-b]){ 
+								judge = false;
+								break;
+							}
+						}
+					}
+					if (judge==true){
+						if (a==0)
+							return new String(origin,0,k-1);
+						else{
+							String optimal2 = splitString(a-1) +" "+new String(origin,a,k);
+							if (optimal2.split(" ").length < optimal.split(" ").length)
+								optimal = optimal2;
+							
+						}
+					}
+				}
+			}
+		}
+		return optimal;
+	}
 
 }
